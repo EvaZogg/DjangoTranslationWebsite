3da1e55385f853f8cfff2b96c1a72dd4f2595f57
EvaZogg <73478751+EvaZogg@users.noreply.github.com>
2020-12-14 15:56:58 +0100

    Problems solved

--- contact/models.py
+++ contact/models.py
@@ -8,5 +8,5 @@ class Contact(models.Model):
   translationText = models.TextField(null=True)
   date = models.DateField()
   # file = models.FileField(null=True)
-  user = models.ForeignKey(User, on_delete=models.CASCADE, null=False) # Represents relationship to logged in User
+  user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) # Represents relationship to logged in User
 
